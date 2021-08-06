f7cada3c6c55942a97149607efb97975dc6651ec
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-08-06 00:21:01 -0300

    operators

---
+++ Operadores-Atribuicao.php
@@ -0,0 +1,25 @@
+<?php
+
+$itemA = 50;
+$itemA += 25; //$itemA = itemA + 25 (Adicionando ao itemA)
+
+echo $itemA . PHP_EOL;
+
+$itemA = 50;
+$itemA -= 25; //$itemA = $itemA - 25 (Subtraindo do itemA)
+
+echo $itemA . PHP_EOL;
+
+$itemA = 100;
+$itemA /= 2; 
+
+echo $itemA . PHP_EOL;
+
+$itemA = 50;
+$itemA *= 2;
+
+echo $itemA . PHP_EOL;
+
+$itemA %= 2;
+
+echo $itemA . PHP_EOL;
